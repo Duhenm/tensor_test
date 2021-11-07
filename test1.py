4bb508dc08aee4437405e9f10b92069590ea1f32
Duhenm <77613285+Duhenm@users.noreply.github.com>
2021-11-07 21:12:25 +0500

    Проверка таблицы с результатами

--- test1.py
+++ test1.py
@@ -1,40 +1,37 @@
-# import unittest
 import time
-
+import unittest
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-driver = webdriver.Firefox(
-    executable_path='./geckodriver.exe'
-)
-driver.get("http://10.10.10.48:8000/")
-time.sleep(5)
-elem = driver.find_element("csrfmiddlewaretoken")
-time.sleep(5)
-elem.send_keys("Тензор")
-assert "No results found." not in driver.page_source
-elem.send_keys(Keys.RETURN)
-driver.close()
 
 
-#
-# class PythonOrgSearch(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.driver = webdriver.Chrome(
-#             executable_path='./chromedriver.exe'
-#         )
-#
-#     def test_search_in_python_org(self):
-#         driver = self.driver
-#         driver.get("http://www.yandex.ru")
-#         elem = driver.find_element_by_name("input input_size_search input_theme_search")
-#         elem.send_keys("Тензор")
-#         assert "No results found." not in driver.page_source
-#         elem.send_keys(Keys.RETURN)
-#
-#     def tearDown(self):
-#         self.driver.close()
-#
-# if __name__ == "__main__":
-#     unittest.main()
+
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(
+            executable_path='./chromedriver.exe'
+        )
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.yandex.ru")
+        elem = driver.find_element(By.NAME, "text")
+        elem.send_keys("Тензор")
+        time.sleep(3)
+        try:
+            elem2 = driver.find_element(By.CSS_SELECTOR, '.mini-suggest__item')
+        except TimeoutException:  # проверяем есть ли таблица с подсказками
+            print('Нет таблицы с подсказками')
+
+        elem.send_keys(Keys.ENTER)
+        time.sleep(5)
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
