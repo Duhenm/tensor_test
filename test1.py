2581f5e14b9c35624aa1049c7939da12ef56f27a
Duhenm <77613285+Duhenm@users.noreply.github.com>
2021-11-07 14:40:10 +0500

    sgrd

--- test1.py
+++ test1.py
@@ -1,14 +1,18 @@
 # import unittest
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome(
-    executable_path='./chromedriver.exe'
+driver = webdriver.Firefox(
+    executable_path='./geckodriver.exe'
 )
-driver.get("http://www.yandex.ru")
-elem = driver.find_element_by_tag_name("search2__input")
+driver.get("http://10.10.10.48:8000/")
+time.sleep(5)
+elem = driver.find_element("csrfmiddlewaretoken")
+time.sleep(5)
 elem.send_keys("Тензор")
-# assert "No results found." not in driver.page_source
+assert "No results found." not in driver.page_source
 elem.send_keys(Keys.RETURN)
 driver.close()
 
